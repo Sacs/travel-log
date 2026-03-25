fd79221ff8f611ccbb74006bd545be0197b063cb
Colin Bell <cbell@vixio.com>
2026-03-25 15:11:29 +0000

    Added landing page for signed out user

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,5 @@
 <template>
-  <div>
-    <h1 class="font-bold text-3xl underline">
-      Hello world!
-    </h1>
-    <button class="btn btn-secondary">
-      View Map <Icon name="tabler:map" />
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
