97da03b61bfe789edf038a7022bc1537bd488858
Colin Bell <cbell@vixio.com>
2026-03-31 13:17:58 +0100

    Implemented github oauth

---
+++ app/components/auth-button.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useAuthStore } from './app/stores/auth';
+
+const authStore = useAuthStore();
+</script>
+
+<template>
+  <button class="btn btn-accent" @click="authStore.signIn">
+    Sign In with Github
+    <span v-if="authStore.loading" class="loading loading-spinner loading-md" />
+    <Icon
+      v-else
+      name="tabler:brand-github"
+      size="24"
+    />
+  </button>
+</template>
--- server/db/schema/auth.ts
+++ server/db/schema/auth.ts
@@ -1,26 +1,26 @@
 import { relations } from 'drizzle-orm';
-import { index, int, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { index, integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 export const user = sqliteTable('user', {
-  id: int().primaryKey({ autoIncrement: true }),
+  id: text().primaryKey(),
   name: text().notNull(),
   email: text().notNull().unique(),
-  emailVerified: int({ mode: 'boolean' })
+  emailVerified: integer({ mode: 'boolean' })
     .default(false)
     .notNull(),
   image: text(),
-  createdAt: int().notNull(),
-  updatedAt: int().notNull(),
+  createdAt: integer({ mode: 'timestamp' }).notNull(),
+  updatedAt: integer({ mode: 'timestamp' }).notNull(),
 });
 
 export const session = sqliteTable(
   'session',
   {
-    id: int().primaryKey({ autoIncrement: true }),
-    expiresAt: int().notNull(),
+    id: text().primaryKey(),
+    expiresAt: integer({ mode: 'timestamp' }).notNull(),
     token: text('token').notNull().unique(),
-    createdAt: int().notNull(),
-    updatedAt: int().notNull(),
+    createdAt: integer({ mode: 'timestamp' }).notNull(),
+    updatedAt: integer({ mode: 'timestamp' }).notNull(),
     ipAddress: text(),
     userAgent: text(),
     userId: text()
@@ -31,28 +31,28 @@ export const session = sqliteTable(
 );
 
 export const account = sqliteTable('account', {
-  id: int().primaryKey({ autoIncrement: true }),
+  id: text().primaryKey(),
   accountId: text().notNull(),
   providerId: text().notNull(),
   userId: text().notNull().references(() => user.id, { onDelete: 'cascade' }),
   accessToken: text(),
   refreshToken: text(),
   idToken: text(),
-  accessTokenExpiresAt: int(),
-  refreshTokenExpiresAt: int(),
+  accessTokenExpiresAt: integer({ mode: 'timestamp' }),
+  refreshTokenExpiresAt: integer({ mode: 'timestamp' }),
   scope: text(),
   password: text(),
-  createdAt: int().notNull(),
-  updatedAt: int().notNull(),
+  createdAt: integer({ mode: 'timestamp' }).notNull(),
+  updatedAt: integer({ mode: 'timestamp' }).notNull(),
 });
 
 export const verification = sqliteTable('verification', {
-  id: int().primaryKey({ autoIncrement: true }),
+  id: text().primaryKey(),
   identifier: text().notNull(),
   value: text().notNull(),
-  expiresAt: int().notNull(),
-  createdAt: int(),
-  updatedAt: int(),
+  expiresAt: integer({ mode: 'timestamp' }).notNull(),
+  createdAt: integer({ mode: 'timestamp' }),
+  updatedAt: integer({ mode: 'timestamp' }),
 });
 
 export const userRelations = relations(user, ({ many }) => ({
